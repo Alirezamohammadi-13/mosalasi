97ecfd8dd0927772d45efe7d2bec78fefbc0d70a
Alirezamohammadi-13 <149057897+Alirezamohammadi-13@users.noreply.github.com>
2023-11-02 16:52:59 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Alireza mohammadi");
+Console.WriteLine("adad fibonachi");
+
+
+Console.WriteLine("adad chandom fibonachi ra namayesh daham?");
+
+int a = 0;
+int b = 1;
+int c = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+for (int i = 0; i < n - 1; i++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    Console.WriteLine(c);
+}
+Console.WriteLine(" adad mored nazar shoma:");
+Console.WriteLine(c);
+Console.WriteLine("tamam");
